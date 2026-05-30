7b68fcf9ad2e2f7c6da93fcbdf1e5e774f9aebe1
Oleg Dronov <oleg.dronov98@gmail.com>
2026-05-30 16:11:29 +0300

    Добавлены тесты для страницы авторизации

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,18 @@
 from selenium.webdriver.common.by import By
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = f"http://selenium1py.pythonanywhere.com/en/"
-    page = MainPage(browser, link)
-    page.open()
+    # mainPageLink = f"http://selenium1py.pythonanywhere.com/"
+    loginPageLink = f"http://selenium1py.pythonanywhere.com/ru/accounts/login/" 
+
+    #page = MainPage(browser, mainPageLink)
+    loginPage = LoginPage(browser, loginPageLink)
+    loginPage.open()
+
     #page.go_to_main_page()
-    page.should_be_login_link()
+    #page.should_be_login_link()
+
+    loginPage.should_be_login_url()
+    loginPage.should_be_login_form()
+    loginPage.should_be_register_form()
